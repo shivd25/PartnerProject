ff930874d9415ee0124d0adfe9db5bb0ef32041f
thealcumus <shivdh57@yahoo.com>
2024-11-27 13:14:49 -0500

    Added Main Logic with Lambda Expression
    Implemented lambda expression for adding numbers in a range through command line arguments.

--- Task4.java
+++ Task4.java
@@ -1,13 +1,26 @@
-public class Task4()
+import java.util.stream.IntStream;
+
+public class Task4
 {
 
 	public static void main(String[] args)
 	{
-	
-	
-	
-	}
+		if(args.length != 2)
+		{
+			System.out.println("Give a start and end integer value.");
+			return;
+		}
 
+		try{
+			int start = Integer.parseInt(args[0]);
+			int end = Integer.parseInt(args[1]);
+			addFirstToLast nums = (a, b) -> IntStream.rangeClosed(a,b).sum();
+			int result = nums.add(start, end);
+			System.out.println("Sum: " + result);
+		}catch (NumberFormatException e) {
+			System.out.println("Invalid input");
+		}
+	}
 
 
 
